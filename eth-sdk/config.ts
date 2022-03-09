b1244b111f9957e09bf96399744615a765700445
Christian Koopmann <christian@MTMAC0057.local>
2022-03-09 13:14:43 +0800

    Calculate path using uniswap smart router

--- eth-sdk/config.ts
+++ eth-sdk/config.ts
@@ -7,10 +7,19 @@ export default defineConfig({
         polygon: {
             exchangeIssuanceLeveraged:
                 "0x600d9950c6ecAef98Cc42fa207E92397A6c43416",
+            tokens: {
+                weth: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+                usdc: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+            },
+            uniswap: {
+                v3: {
+                    pool: "0x45dDa9cb7c25131DF268515131f647d726f50608",
+                },
+            },
         },
     },
     etherscanKey: process.env.POLYGONSCAN_API_KEY,
     rpc: {
-        polygon: process.env.POLYGON_URL
-    }
+        polygon: process.env.POLYGON_URL,
+    },
 });
--- scripts/issue.ts
+++ scripts/issue.ts
@@ -4,19 +4,87 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers, network } from "hardhat";
-import { getPolygonSdk } from '@dethcrypto/eth-sdk-client'
+import { getPolygonSdk } from "@dethcrypto/eth-sdk-client";
+import { AlphaRouter } from "@uniswap/smart-order-router";
+import { Token, CurrencyAmount, Percent, TradeType } from "@uniswap/sdk-core";
+import { Wallet, Signer } from "ethers";
+import JSBI from "jsbi";
 
 async function main() {
     const signers = await ethers.getSigners();
-    const defaultSigner = signers[0];
+    const signer = signers[0];
 
-    const polygonSdk = getPolygonSdk(defaultSigner)
-    console.log("UniV3Router", await polygonSdk.exchangeIssuanceLeveraged.uniV3Router())
+    if (signer == null) {
+        throw new Error(
+            "Signer undefined, remember to set private key when running against mainnet"
+        );
+    }
+    console.log("Signer: ", signer.address);
+    const chainId = 137;
+
+    const routerConfig = {
+        chainId,
+        provider: ethers.provider,
+    };
+
+    const polygonSdk = getPolygonSdk(signer);
+    console.log(
+        "UniV3Router",
+        await polygonSdk.exchangeIssuanceLeveraged.uniV3Router()
+    );
+
+    const router = new AlphaRouter(routerConfig);
+
+    const weth = new Token(
+        chainId,
+        polygonSdk.tokens.weth.address,
+        18,
+        "weth",
+        "wrapped ether"
+    );
+    const usdc = new Token(
+        chainId,
+        polygonSdk.tokens.usdc.address,
+        6,
+        "usdc",
+        "usd//c"
+    );
+
+    const typedValueParsed = "100000000000000000000";
+    const wethAmount = CurrencyAmount.fromRawAmount(
+        weth,
+        JSBI.BigInt(typedValueParsed)
+    );
+
+    const route = await router.route(
+        wethAmount,
+        usdc,
+        TradeType.EXACT_INPUT,
+        {
+            recipient: signer.address,
+            slippageTolerance: new Percent(5, 100),
+            deadline: Date.now() + 1800,
+        },
+        { maxSplits: 1 }
+    );
+    const tokenPath = route?.route[0].tokenPath.map((token) => token.address);
+    console.log("TokenPath: ", tokenPath);
+    const feePromises = route?.route[0].poolAddresses.map(
+        async (poolAddress) => {
+            const contract = polygonSdk.uniswap.v3.pool.attach(poolAddress);
+            const fee = await contract.fee();
+            return fee;
+        }
+    );
+    if (feePromises !== undefined) {
+        const fees = await Promise.all(feePromises);
+        console.log("Fees: ", fees);
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
